f8195c25845d0722f0a1620b9cb6227115cbf575
kawther1010 <kawthernfs@protonmail.com>
2024-01-05 22:47:08 +0100

    a simple university project that i did in my 3rd year of university as a SIQ student

---
+++ app/src/main/java/com/example/app01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.app01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       btn= findViewById(R.id.button2);
+    }
+}
